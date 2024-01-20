9d75d6db688a1b4242163f4a6f4c3cf542a2ce11
AlexDv <DvoinikovAV@yandex.ru>
2024-01-20 11:34:30 +0300

    Домашняя работа 6 задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //Задание 1
+        byte i;
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
